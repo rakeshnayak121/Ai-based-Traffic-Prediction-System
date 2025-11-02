998a72b8d32812924e63ab859bbd9f90d902ad94
Rakesh nayak <155887307+rakeshnayak121@users.noreply.github.com>
2025-11-02 13:58:58 +0530

    Update a.py

--- a.py
+++ a.py
@@ -18,10 +18,7 @@ emergency_labels = ['Police Car', 'Police Van', 'Fire Truck', 'Ambulance']
 non_emergency_labels = ['car', 'bus', 'truck', 'motorcycle']
 
 st.title("ADVANCED TRAFFIC FLOW OPTIMIZATION FOR INTELLIGENT TRAFFIC SYSTEM - Emergency Vehicle Detection")
-st.info(
-    "👉 **Upload an image or video of any emergency vehicle to get the detection results.**\n\n"
-    "Supported formats: MP4, MOV, AVI, MKV, JPEG, PNG | Max 200 MB per file"
-)
+
 
 
 # File uploader for multiple videos
@@ -133,6 +130,7 @@ if uploaded_files:
         st.write(f"Estimated Road Clearance Time: {clearance_time} seconds")
 
     st.write(f"### Total Road Clearance Time for All Videos: {total_clearance_time} seconds")
+
 
 
 
